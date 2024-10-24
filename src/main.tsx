32215587e29fce1a7d1ba4e1d6eaa08480f0628e
Eduardo Alvarez <el_alienx@hotmail.com>
2024-10-24 11:21:56 +0200

    adding client

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 
 // Project files
 import Routes from "./Routes";
 import "./main.css";
 
+// Properties
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<h1>Sorry, an error occurred.</h1>}>
-      <Toaster />
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <Routes />
+      </QueryClientProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
